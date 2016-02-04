25cac81f8740a70b139e3e81e534c9569fcc3d9e
Shi Shu <shishu_1998@yahoo.com>
2016-02-04 11:48:04 -0500

    classwork

--- makepic.c
+++ makepic.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+int randNum(){
+  int x = open("/dev/random",O_RDONLY);
+  int *rand = (int*)malloc(4);
+  read(x,rand,4);
+  close(x);
+  return rand % 255;
+}
+
 void makeimage(){
   int x = open("image.ppm",O_CREAT,O_WRONLY,O_TRUNC);
   char* header = "p3\n255 10 255\n";
   write(x,header,sizeof(header));
+  char str[15];
   int counter;
   int counter2;
-  while(counter < 10){
-    while(counter2 < 255){
+  while(counter2 < 10){
+    while(counter < 255){
+      counter ++;
     }
     counter ++;
   }
