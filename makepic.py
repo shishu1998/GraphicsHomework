aecd6b1c553806e6d7dad2963fc23fe4697b60f3
shishu1998 <shishu_1998@yahoo.com>
2016-02-04 21:13:36 -0500

    now in python

--- makepic.py
+++ makepic.py
@@ -1,11 +1,9 @@
-import random
-
 picture = "P3 500 500 255 "
 for y in range(1,500):
     for x in range(1,500):
-        red = (random.randint(0,255))%255
-        green = (random.randint(0,255))%255
-        blue = (random.randint(0,255))%255
+        red = x % 255
+        green = 0
+        blue = y % 255
         picture += "%d %d %d "%(red,green,blue) 
 
         
