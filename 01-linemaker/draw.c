f749d9dd3f9c321922b67308ad546e9caea879ae
Shi Shu <shishu_1998@yahoo.com>
2016-02-19 17:12:38 -0500

    stuff

--- 01-linemaker/draw.c
+++ 01-linemaker/draw.c
@@ -5,11 +5,6 @@
 #include "display.h"
 #include "draw.h"
 
-//plotting method
-void plot(int x, int y, screen s){
-  screen[x][y].red = 255;
-}
-
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   int A = y1 - y0;
@@ -18,54 +13,54 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   int slope = A/ -B;
   //Octant I(0 < slope < 1)
   if(0 < slope && slope < 1){
-    d = 2A + B;
+    d = 2*A + B;
     while(x0 <= x1){
-      plot(x,y);
+      plot(s,c,x0,y0);
       if(d < 0){
       y0 += 1;
-      d += 2B;
+      d += 2*B;
       }
       x0 +=1;
-      d += 2A;
+      d += 2*A;
     }
   }
   //Octant II
   if(slope > 1){
-    d = A + 2B;
+    d = A + 2*B;
     while(y0 <= y1){
-      plot(x,y);
+      plot(s,c,x0,y0);
       if(d < 0){
 	x0 +=1;
-	d += 2A;
+	d += 2*A;
       }
       y0 += 1;
-      d +=2B;
+      d +=2*B;
     }
   }
   //Octant VII
   if(slope < -1){
-    d = -2A + B;
-    while(yo >= y1){
-      plot(x,y);
+    d = -2*A + B;
+    while(y0 >= y1){
+      plot(s,c,x0,y0);
       if(d < 0){
 	x0 += 1;
-	d += 2A;
+	d += 2*A;
       }
       y0 += 1;
-      d += 2B;
+      d += 2*B;
     }
   }
   //Octant VIII 
   if(slope < 0 && slope > -1){
-    d = -A + 2B;
+    d = -A + 2*B;
     while(x0 <= x1){
-      plot(x,y);
+      plot(s,c,x0,y0);
       if(d < 0){
 	y0 += 1;
-	d += 2A;
+	d += 2*A;
       }
       x0 += 1;
-      d += 2B;
+      d += 2*B;
     }
   }
 
