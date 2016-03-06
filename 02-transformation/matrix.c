a98f563172941dc4019a705bdb1a10ebaa9eb61f
Shi Shu <shishu_1998@yahoo.com>
2016-03-06 14:49:43 -0500

    Test

--- 02-transformation/matrix.c
+++ 02-transformation/matrix.c
@@ -149,8 +149,9 @@ void matrix_mult(struct matrix *a, struct matrix *b) {
       sum = 0;
       for(t=0;t < a->cols; t++){
 	sum += a->m[r][t]*b->m[t][c];
+	temp->m[r][c] = sum;
       }
-      temp->m[r][c] = sum;
+      //temp->m[r][c] = sum;
     }
   }
   copy_matrix(temp,b);
